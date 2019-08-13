9efe54d633bfc62a3ad59d32d1087bd1c11a6978
Entony <tony@MacBook-Air-Entony.local>
2019-08-13 20:33:43 +0300

    fix

--- routes/orders.js
+++ routes/orders.js
@@ -6,7 +6,7 @@ const { authenticationMiddleware } = require('../utils/help-func');
 const router = express.Router();
 
 router.get('/', orders.getOrderList);
-router.post('/', authenticationMiddleware, orders.createOrder);
+router.post('/', orders.createOrder);
 router.put('/:id', authenticationMiddleware, orders.updateOrder);
 router.delete('/:id', authenticationMiddleware, orders.removeOrder);
 
