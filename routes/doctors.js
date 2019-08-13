3f0f9d0326504af16692c801d60ca28c8c269223
Entony <tony@MacBook-Air-Entony.local>
2019-08-13 19:30:55 +0300

    fix expe

--- routes/doctors.js
+++ routes/doctors.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', doctor.getDoctorList);
 router.get('/:id', doctor.getDoctorById);
 router.post('/', authenticationMiddleware, doctor.createDoctor);
-router.put('/:id', authenticationMiddleware, doctor.updateDoctor);
+router.put('/:id', doctor.updateDoctor);
 router.delete('/:id', authenticationMiddleware, doctor.removeDoctor);
 
 module.exports = router;
